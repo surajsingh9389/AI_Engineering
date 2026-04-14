bd0f51f293792bc1da5f8eafc54d10336505635d
surajsingh9389 <sk9389623488@gmail.com>
2026-04-14 15:11:59 +0530

    feat: add few shot prompting

--- Prompt Engineering and Guardrails/main.py
+++ Prompt Engineering and Guardrails/main.py
@@ -16,6 +16,7 @@ docs = [
 ]
 
 query = "How long does iPhone refund take?"
+# query = "what is Amazon?"
 
 # BM25 expects a list of lists (each list contains words)
 tokenized_corpus = [doc.lower().split() for doc in docs]
@@ -116,10 +117,37 @@ You are a strict question-answering assistant.
 Rules:
 1. Answer ONLY using the provided context.
 2. Do NOT use any external knowledge.
-3. If the answer is not explicitly present in the context, respond with EXACTLY:
+3. The answer must be explicitly stated in a SINGLE sentence from the context.
+4. Do NOT combine information from multiple sentences.
+5. If the answer is not explicitly present, respond with EXACTLY:
    "I don't know"
-4. Do NOT explain your answer.
-5. Do NOT add any extra text.
+6. Do NOT explain your answer.
+7. Do NOT add any extra text.
+
+------
+
+Example 1:
+Context: iPhone refund policy is 7 days
+Question: What is iPhone refund policy?
+Answer: iPhone refund policy is 7 days
+
+Example 2:
+Context: Refunds are processed in 5 days
+Question: What is the price of iPhone?
+Answer: I don't know
+
+Example 3:
+Context:
+Refunds are processed within 5 days
+iPhone 15 refund policy is 7 days
+
+Question:
+How long does iPhone refund take?
+
+Answer:
+I don't know
+
+------
 
 Context:
 {context}
