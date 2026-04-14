5cd64d0cbd4e5b66256cb56359d6bae69fe379c3
surajsingh9389 <sk9389623488@gmail.com>
2026-04-14 16:44:06 +0530

    fix: add rule to handle lexical ambiguity

--- Prompt Engineering and Guardrails/main.py
+++ Prompt Engineering and Guardrails/main.py
@@ -5,108 +5,108 @@ from huggingface_hub import InferenceClient
 import os
 import numpy as np
 
-# ------------ Using Bm25 the top matching ---------------
+# # ------------ Using Bm25 the top matching ---------------
 
-docs = [
-  "Samsung return policy is 10 days",
-  "Refunds are processed within 5 days",
-  "iPhone 15 refund policy is 7 days",
-  "The Amazon river is the largest by discharge volume.",
-  "Amazon.com is a leading e-commerce company."
-]
+# docs = [
+#   "Samsung return policy is 10 days",
+#   "Refunds are processed within 5 days",
+#   "iPhone 15 refund policy is 7 days",
+#   "The Amazon river is the largest by discharge volume.",
+#   "Amazon.com is a leading e-commerce company."
+# ]
 
-query = "How long does iPhone refund take?"
-# query = "what is Amazon?"
-
-# BM25 expects a list of lists (each list contains words)
-tokenized_corpus = [doc.lower().split() for doc in docs]
-
-# Initialize the BM25 object
-bm25 = BM25Okapi(tokenized_corpus)
-
-# Tokenize your query
-tokenized_query = query.lower().split()
-
-# Get scores or the top results
-doc_scores = bm25.get_scores(tokenized_query)
-top_n = bm25.get_top_n(tokenized_query, docs, n=3)
-
-bm25_results = top_n
-# print(bm25_results)
-
-# print(f"Scores: {doc_scores}")
-# print(f"Best Match: {top_n}")
+# query = "Explain Amazon"
 
 
-# --------------- Using Faiss the top matching ----------------
+# # BM25 expects a list of lists (each list contains words)
+# tokenized_corpus = [doc.lower().split() for doc in docs]
 
-# 1. Load a model
-model = SentenceTransformer('all-MiniLM-L6-v2')
+# # Initialize the BM25 object
+# bm25 = BM25Okapi(tokenized_corpus)
 
-# Generate embeddings
-embedding_docs = model.encode(docs)
-embedding_query = model.encode(query)
+# # Tokenize your query
+# tokenized_query = query.lower().split()
 
-embedding_docs = np.array(embedding_docs).astype('float32')
-embedding_query = np.array([embedding_query]).astype('float32')
+# # Get scores or the top results
+# doc_scores = bm25.get_scores(tokenized_query)
+# top_n = bm25.get_top_n(tokenized_query, docs, n=3)
 
-# normalize Embeddings 
-faiss.normalize_L2(embedding_docs)
-faiss.normalize_L2(embedding_query)
+# bm25_results = top_n
+# # print(bm25_results)
 
-d = embedding_docs.shape[1]
-
-index = faiss.IndexFlatIP(d)
-
-index.add(embedding_docs)
-k = 3
-
-distance, indices= index.search(embedding_query, k)
-
-faiss_results = [docs[i] for i in indices[0]]
-
-# print(faiss_results)
-
-# for j, i in enumerate(indices[0]):
-#   print(f"score: {distance[0][j]}, sentence: {docs[i]}")
+# # print(f"Scores: {doc_scores}")
+# # print(f"Best Match: {top_n}")
 
 
-# ------------- RRf algorithm ----------------
+# # --------------- Using Faiss the top matching ----------------
 
-def rrf(bm25_results, faiss_results, k=60):
-  scores={}
+# # 1. Load a model
+# model = SentenceTransformer('all-MiniLM-L6-v2')
+
+# # Generate embeddings
+# embedding_docs = model.encode(docs)
+# embedding_query = model.encode(query)
+
+# embedding_docs = np.array(embedding_docs).astype('float32')
+# embedding_query = np.array([embedding_query]).astype('float32')
+
+# # normalize Embeddings 
+# faiss.normalize_L2(embedding_docs)
+# faiss.normalize_L2(embedding_query)
+
+# d = embedding_docs.shape[1]
+
+# index = faiss.IndexFlatIP(d)
+
+# index.add(embedding_docs)
+# k = 3
+
+# distance, indices= index.search(embedding_query, k)
+
+# faiss_results = [docs[i] for i in indices[0]]
+
+# # print(faiss_results)
+
+# # for j, i in enumerate(indices[0]):
+# #   print(f"score: {distance[0][j]}, sentence: {docs[i]}")
+
+
+# # ------------- RRf algorithm ----------------
+
+# def rrf(bm25_results, faiss_results, k=60):
+#   scores={}
   
-  # BM25 contribution
-  for rank, doc in enumerate(bm25_results, 1):
-    scores[doc] = scores.get(doc, 0) + 1/(rank+k)
+#   # BM25 contribution
+#   for rank, doc in enumerate(bm25_results, 1):
+#     scores[doc] = scores.get(doc, 0) + 1/(rank+k)
     
-  # Faiss contribution
-  for rank, doc in enumerate(faiss_results, 1):
-    scores[doc] = scores.get(doc, 0) + 1/(rank+k)
+#   # Faiss contribution
+#   for rank, doc in enumerate(faiss_results, 1):
+#     scores[doc] = scores.get(doc, 0) + 1/(rank+k)
     
-  return sorted(scores.items(), key=lambda item: item[1], reverse=True)
+#   return sorted(scores.items(), key=lambda item: item[1], reverse=True)
 
 
-hybrid_results = rrf(bm25_results, faiss_results)
+# hybrid_results = rrf(bm25_results, faiss_results)
 
-# print(f"Hybrid Results: {hybrid_results}")
+# # print(f"Hybrid Results: {hybrid_results}")
 
-# ------------- Re-Ranking ----------------
+# # ------------- Re-Ranking ----------------
 
-# Convert into pairs(query, retrieve_docs)
-pairs = [(query, doc) for doc, _ in hybrid_results]
+# # Convert into pairs(query, retrieve_docs)
+# pairs = [(query, doc) for doc, _ in hybrid_results]
 
-# print(f"Query + docs: {pairs}")
+# # print(f"Query + docs: {pairs}")
 
-# Load model 
-model = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2')
+# # Load model 
+# model = CrossEncoder('cross-encoder/ms-marco-MiniLM-L-6-v2')
 
-# calculate scores for re-ranking 
-scores = model.predict(pairs)
+# # calculate scores for re-ranking 
+# scores = model.predict(pairs)
 
-sorted_docs = sorted(zip([doc for doc, _ in hybrid_results], scores), key=lambda x: x[1], reverse=True)
+# sorted_docs = sorted(zip([doc for doc, _ in hybrid_results], scores), key=lambda x: x[1], reverse=True)
 
-print(f"After Re-Ranking docs and scores: {sorted_docs}")
+# print(f"After Re-Ranking docs and scores: {sorted_docs}")
 
 
 def build_prompt(context):
@@ -123,6 +123,7 @@ Rules:
    "I don't know"
 6. Do NOT explain your answer.
 7. Do NOT add any extra text.
+8. If multiple context sentences provide different possible answers, respond with EXACTLY: "I don't know"
 
 ------
 
@@ -140,10 +141,17 @@ Example 3:
 Context:
 Refunds are processed within 5 days
 iPhone 15 refund policy is 7 days
-
 Question:
 How long does iPhone refund take?
+Answer:
+I don't know
 
+Example 4:
+Context:
+The Amazon river is the largest by discharge volume
+Amazon.com is a leading e-commerce company
+Question:
+What is Amazon?
 Answer:
 I don't know
 
@@ -155,9 +163,16 @@ Context:
     
   return prompt
 
-# Retrieve context 
-retrieve_docs = [doc for doc, _ in sorted_docs]
-context = "\n".join(retrieve_docs)
+# # Retrieve context 
+# retrieve_docs = [doc for doc, _ in sorted_docs]
+# context = "\n".join(retrieve_docs)
+
+context = """
+  Java is a high-level, class-based, object-oriented programming language.
+Java is an island in Indonesia, home to over 140 million people.
+"""
+
+query = "What is Java?"
 
 # Generate prompt 
 prompt = build_prompt(context)
